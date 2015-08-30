8262b4528eb68bd83e7cf14463f294379dba855a
André Willik Valenti <awvalenti@gmail.com>
2015-08-30 13:03:17 -0300

    forficata-implementation-wiimotej module

---
+++ forficata-implementation-wiimotej/src/main/java/com/github/awvalenti/forficata/implementation/wiimotej/WiimoteJWiimoteConnector.java
@@ -0,0 +1,16 @@
+package com.github.awvalenti.forficata.implementation.wiimotej;
+
+import com.github.awvalenti.forficata.api.WiimoteConnectedCallback;
+import com.github.awvalenti.forficata.api.WiimoteConnector;
+
+public class WiimoteJWiimoteConnector implements WiimoteConnector {
+
+	@Override
+	public void searchAndConnect(final WiimoteConnectedCallback callback) {
+	}
+
+	@Override
+	public void connectToWiimoteAt(String bluetoothAddress, WiimoteConnectedCallback callback) {
+	}
+
+}
