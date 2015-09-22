1ab4fba3eea32cb7ce8dba9d9945cc07444ba7a0
André Willik Valenti <awvalenti@gmail.com>
2015-09-22 14:58:02 -0300

    NitidaWindow: colors for states

--- nitida/src/main/java/com/github/awvalenti/bauhinia/nitida/NitidaWindow.java
+++ nitida/src/main/java/com/github/awvalenti/bauhinia/nitida/NitidaWindow.java
@@ -1,13 +1,19 @@
 package com.github.awvalenti.bauhinia.nitida;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.RenderingHints;
 
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
 
 public class NitidaWindow {
 
@@ -21,27 +27,66 @@ public class NitidaWindow {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		String[] states = { "Idle", "Connecting", "Active" };
+		Color[] colors = { Color.RED, Color.YELLOW, Color.GREEN };
 
 		JPanel state = new JPanel();
-		state.setBorder(new TitledBorder("State"));
+		state.setBorder(BorderFactory.createTitledBorder("State"));
 		state.setLayout(new GridLayout(states.length, 1));
-		for (String s : states) {
-			state.add(new JLabel(s));
+		for (int i = 0; i < states.length; ++i) {
+			final JLabel label = new JLabel(states[i]);
+			label.setBorder(BorderFactory.createEtchedBorder());
+			label.setIcon(new StateIcon(colors[i]));
+			label.setOpaque(true);
+			label.setDisabledIcon(new StateIcon(colors[i].darker().darker().darker()));
+			label.setEnabled(false);
+			state.add(label);
 		}
 		frame.add(state, BorderLayout.NORTH);
 
-		JPanel actions = new JPanel();
-		actions.setBorder(new TitledBorder("Actions"));
-		actions.add(new JButton("Reconnect"));
-		frame.add(actions, BorderLayout.SOUTH);
-
 		JPanel log = new JPanel(new BorderLayout());
-		log.setBorder(new TitledBorder("Log"));
+		log.setBorder(BorderFactory.createTitledBorder("Log"));
 		frame.add(log, BorderLayout.CENTER);
+
+		JButton reconnect = new JButton("Reconnect");
+		reconnect.setEnabled(false);
+
+		JPanel actions = new JPanel();
+		actions.setBorder(BorderFactory.createTitledBorder("Actions"));
+		actions.add(reconnect);
+		frame.add(actions, BorderLayout.SOUTH);
 	}
 
 	public void run() {
 		frame.setVisible(true);
+	}
+
+	private static class StateIcon implements Icon {
+		private final Color color;
+
+		public StateIcon(Color color) {
+			this.color = color;
+		}
+
+		@Override
+		public void paintIcon(Component c, Graphics g, int x, int y) {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+			g2d.setColor(Color.BLACK);
+			g2d.fillRect(6, 6, 24, 24);
+			g2d.setColor(color);
+			g2d.fillOval(10, 10, 16, 16);
+		}
+
+		@Override
+		public int getIconWidth() {
+			return 32;
+		}
+
+		@Override
+		public int getIconHeight() {
+			return 32;
+		}
 	}
 
 }
