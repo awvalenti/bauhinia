c568fdeb447565ff48e9acf46a32994c4f06042e
André Willik Valenti <awvalenti@gmail.com>
2015-09-24 15:48:29 -0300

    WiiuseJ implementation using ForficataCallback

--- forficata-implementation-wiiusej/src/main/java/com/github/awvalenti/bauhinia/forficata/implementation/wiiusej/WiiuseJWiimoteConnector.java
+++ forficata-implementation-wiiusej/src/main/java/com/github/awvalenti/bauhinia/forficata/implementation/wiiusej/WiiuseJWiimoteConnector.java
@@ -3,6 +3,7 @@ package com.github.awvalenti.bauhinia.forficata.implementation.wiiusej;
 import wiiusej.WiiUseApiManager;
 
 import com.github.awvalenti.bauhinia.forficata.api.ForficataCallback;
+import com.github.awvalenti.bauhinia.forficata.api.ForficataException;
 import com.github.awvalenti.bauhinia.forficata.api.WiimoteConnector;
 
 public class WiiuseJWiimoteConnector implements WiimoteConnector {
@@ -15,9 +16,26 @@ public class WiiuseJWiimoteConnector implements WiimoteConnector {
 
 	@Override
 	public void run(final ForficataCallback callback) {
-		wiiusej.Wiimote[] wiimotesFound = WiiUseApiManager.getWiimotes(maximumNumberOfWiimotes, false);
-		for (wiiusej.Wiimote w : wiimotesFound) {
-			callback.wiimoteConnected(new WiiuseJWiimoteAdapter(w));
+		try {
+			// This loads WiiuseJ classes and libraries
+			WiiUseApiManager.getInstance();
+
+			callback.searchStarted();
+			wiiusej.Wiimote[] wiimotesFound = WiiUseApiManager.getWiimotes(maximumNumberOfWiimotes,
+					false);
+			for (wiiusej.Wiimote w : wiimotesFound) {
+				callback.wiimoteConnected(new WiiuseJWiimoteAdapter(w));
+			}
+			callback.searchFinished();
+
+		} catch (ExceptionInInitializerError e) {
+			// This happens when WiiuseJ fails to load native libraries.
+			// Although catching this error is not a great thing to do, for current version of
+			// WiiuseJ, it is the only alternative to find out that a problem occurred with native
+			// libraries.
+
+			// TODO Use exception factory
+			callback.errorOccurred(new ForficataException(e, "Error loading native libraries"));
 		}
 	}
 
