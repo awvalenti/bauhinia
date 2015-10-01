4ed7844552758fb19b1fe911de15f6419212d381
André Willik Valenti <awvalenti@gmail.com>
2015-10-01 10:23:21 -0300

    Small refactorings

--- nitida/src/main/java/com/github/awvalenti/bauhinia/nitida/NitidaMain.java
+++ nitida/src/main/java/com/github/awvalenti/bauhinia/nitida/NitidaMain.java
@@ -19,16 +19,15 @@ public class NitidaMain {
 
 	public static void main(String[] args) {
 		if (args.length > 0 && args[0].equals("--console")) runNitidaConsole();
-		else runNitidaWindow(new ProjectProperties());
+		else runNitidaWindow();
 	}
 
-	private static void runNitidaWindow(ProjectProperties projectProperties) {
-		NitidaModel model;
+	private static void runNitidaWindow() {
 		ForficataBuilderStep3 builder = Forficata.guidedBuilder()
 				.asynchronous()
 				.oneWiimote();
 
-		model = new NitidaModel(builder);
+		NitidaModel model = new NitidaModel(builder);
 
 		RetryButton retryButton = new RetryButton();
 		PhasePanel phasePanel = new PhasePanel(Phase.values());
@@ -41,22 +40,26 @@ public class NitidaMain {
 				.phaseStateObserver(phasePanel)
 				.fullObserver(logPanel);
 
-		model.setConnector(builder.build());
-
-		NitidaWindow nitidaWindow = new NitidaWindow(projectProperties,
+		NitidaWindow nitidaWindow = new NitidaWindow(new ProjectProperties(),
 				new ApplicationStatePanel(phasePanel, macroStatePanel), logPanel,
 				new ActionPanel(retryButton));
 
 		new NitidaController(model, retryButton);
 		nitidaWindow.run();
+
+		model.setConnector(builder.build());
 		model.connect();
 	}
 
 	private static void runNitidaConsole() {
-		NitidaModel model;
-		ForficataBuilderStep3 builder = Forficata.guidedBuilder().synchronous().oneWiimote();
-		model = new NitidaModel(builder);
+		ForficataBuilderStep3 builder = Forficata.guidedBuilder()
+				.synchronous()
+				.oneWiimote();
+
+		NitidaModel model = new NitidaModel(builder);
+
 		builder.fullObserver(new NitidaConsole(new ProjectProperties()));
+
 		model.setConnector(builder.build());
 		model.connect();
 	}
