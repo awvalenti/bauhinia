979dfb2649a4bd7ba5c27795217926fdd2d686f6
André Willik Valenti <awvalenti@gmail.com>
2015-10-02 14:40:49 -0300

    Improving connection state indications

--- nitida/src/main/java/com/github/awvalenti/bauhinia/nitida/view/window/ConnectionStateIndication.java
+++ nitida/src/main/java/com/github/awvalenti/bauhinia/nitida/view/window/ConnectionStateIndication.java
@@ -1,5 +1,6 @@
 package com.github.awvalenti.bauhinia.nitida.view.window;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
@@ -9,54 +10,78 @@ import java.awt.RenderingHints;
 import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.CompoundBorder;
 
-public class ConnectionStateIndication extends JLabel {
+public class ConnectionStateIndication extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
 	private final Color originalBackgroundColor;
 
-	public ConnectionStateIndication(String title, Color color) {
-		super(title, new CircleIcon(color), JLabel.LEFT);
-		originalBackgroundColor = getBackground();
-		setDisabledIcon(new CircleIcon(color.darker().darker()));
-		setBorder(BorderFactory.createEtchedBorder());
+	private JLabel iconLabel;
+	private JLabel textLabel;
+
+	public ConnectionStateIndication(String title, Color enabledColor) {
+		setLayout(new BorderLayout());
+		setBorder(new CompoundBorder(BorderFactory.createEtchedBorder(),
+				BorderFactory.createEmptyBorder(5, 0, 5, 0)));
 		setOpaque(true);
-		setEnabled(false);
+
+		iconLabel = new JLabel(new CircleIcon(enabledColor));
+		iconLabel.setDisabledIcon(new CircleIcon(enabledColor.darker().darker()));
+		iconLabel.setOpaque(true);
+
+		originalBackgroundColor = iconLabel.getBackground();
+
+		textLabel = new JLabel(title, JLabel.CENTER);
+		textLabel.setOpaque(true);
+
+		add(iconLabel, BorderLayout.NORTH);
+		add(textLabel, BorderLayout.SOUTH);
 	}
 
 	@Override
 	public final void setEnabled(boolean enabled) {
-		super.setEnabled(enabled);
+		iconLabel.setEnabled(enabled);
+		textLabel.setEnabled(enabled);
 		setBackground(enabled ? Color.WHITE : originalBackgroundColor);
+		iconLabel.setBackground(enabled ? Color.WHITE : originalBackgroundColor);
+		textLabel.setBackground(enabled ? Color.WHITE : originalBackgroundColor);
 	}
 
 	private static class CircleIcon implements Icon {
 
-		private final Color color;
+		private final Color innerCircleColor;
 
-		public CircleIcon(Color color) {
-			this.color = color;
+		public CircleIcon(Color innerCircleColor) {
+			this.innerCircleColor = innerCircleColor;
 		}
 
 		@Override
 		public void paintIcon(Component c, Graphics g, int x, int y) {
 			Graphics2D g2d = (Graphics2D) g;
 			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+			int startingX = (c.getWidth() - getIconWidth()) / 2, outerCircleRadius = 25,
+					innerCircleRadius = 15, distanceBetweenCircles = (outerCircleRadius - innerCircleRadius) / 2;
+
 			g2d.setColor(Color.BLACK);
-			g2d.fillRect(6, 6, 24, 24);
-			g2d.setColor(color);
-			g2d.fillOval(10, 10, 16, 16);
+			g2d.fillOval(startingX, 0, outerCircleRadius, outerCircleRadius);
+
+			g2d.setColor(innerCircleColor);
+			g2d.fillOval(startingX + distanceBetweenCircles, distanceBetweenCircles,
+					innerCircleRadius, innerCircleRadius);
 		}
 
 		@Override
 		public int getIconWidth() {
-			return 28;
+			return 25;
 		}
 
 		@Override
 		public int getIconHeight() {
-			return 32;
+			return 28;
 		}
 
 	}
