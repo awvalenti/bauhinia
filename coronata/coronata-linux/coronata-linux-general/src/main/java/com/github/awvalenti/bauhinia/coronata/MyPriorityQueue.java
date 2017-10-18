8fbc462f99cc964ea4ad3542ff82ebb3d6afa377
Andre Willik Valenti <awvalenti@gmail.com>
2017-10-17 22:08:35 -0200

    Removing public modifier from implementation classes

--- coronata/coronata-linux/coronata-linux-general/src/main/java/com/github/awvalenti/bauhinia/coronata/MyPriorityQueue.java
+++ coronata/coronata-linux/coronata-linux-general/src/main/java/com/github/awvalenti/bauhinia/coronata/MyPriorityQueue.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class MyPriorityQueue<E extends Comparable<E>> implements Iterable<E> {
+class MyPriorityQueue<E extends Comparable<E>> implements Iterable<E> {
 
 	private final ArrayList<E> sortedElements = new ArrayList<E>();
 
-	public void push(E t) {
+	void push(E t) {
 		sortedElements.add(indexFor(t), t);
 	}
 
-	public E pop() {
+	E pop() {
 		if (isEmpty()) throw new NoSuchElementException("Empty list");
 		return sortedElements.remove(0);
 	}
 
-	public final boolean isEmpty() {
+	final boolean isEmpty() {
 		return sortedElements.isEmpty();
 	}
 
