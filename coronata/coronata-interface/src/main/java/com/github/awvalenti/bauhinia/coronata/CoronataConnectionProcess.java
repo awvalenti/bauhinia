dd5c984402b0fc445985ca034ed2e8558082b3e9
Andre Willik Valenti <awvalenti@gmail.com>
2017-11-10 08:20:34 -0200

    Improving javadoc

--- coronata/coronata-interface/src/main/java/com/github/awvalenti/bauhinia/coronata/CoronataConnectionProcess.java
+++ coronata/coronata-interface/src/main/java/com/github/awvalenti/bauhinia/coronata/CoronataConnectionProcess.java
@@ -5,7 +5,7 @@ public interface CoronataConnectionProcess {
 	void start();
 
 	/**
-	 * Works on Linux, whose connection process takes about 5 to 30 seconds.
+	 * Works on Linux, whose connection process takes about 10 to 30 seconds.
 	 * On Windows, has no effect, for two reasons: Windows connection process
 	 * 1) is controlled by an underlying library; 2) usually takes less than a
 	 * second to run.
